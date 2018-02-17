40db7775b76debf2bbb101a783341594f0b61e05
jaryaman <j.aryaman25@gmail.com>
2018-02-17 20:20:37 +0000

    Add contours

--- Notebooks/utls.py
+++ Notebooks/utls.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import numpy as np
 
 def reset_plots():
 	plt.close('all')
@@ -11,3 +12,22 @@ def reset_plots():
 	mpl.rc('lines', markersize=10)
 	plt.rcParams.update({'axes.labelsize': fontsize})
 	mpl.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}', r'\usepackage{amsfonts}']
+
+def multivariate_gaussian(pos, mu, Sigma):
+    """Return the multivariate Gaussian distribution on array pos.
+
+    pos is an array constructed by packing the meshed arrays of variables
+    x_1, x_2, x_3, ..., x_k into its _last_ dimension.
+
+    Source: https://scipython.com/blog/visualizing-the-bivariate-gaussian-distribution/
+    """
+
+    n = mu.shape[0]
+    Sigma_det = np.linalg.det(Sigma)
+    Sigma_inv = np.linalg.inv(Sigma)
+    N = np.sqrt((2*np.pi)**n * Sigma_det)
+    # This einsum call calculates (x-mu)T.Sigma-1.(x-mu) in a vectorized
+    # way across all the input variables.
+    fac = np.einsum('...k,kl,...l->...', pos-mu, Sigma_inv, pos-mu)
+
+    return np.exp(-fac / 2) / N
